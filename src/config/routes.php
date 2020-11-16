009a03d19b236c89729132b68c6da4cba5177ee4
tropotek <info@tropotek.com>
2020-11-16 11:51:12 +1100

    ~Auto: Commit

--- src/config/routes.php
+++ src/config/routes.php
@@ -14,64 +14,64 @@
 use \Tk\Routing\Route;
 
 $config = \App\Config::getInstance();
-$Routes = $config->getRouteCollection();
-if (!$Routes) return;
+$routes = $config->getRouteCollection();
+if (!$routes) return;
 
 
 // Default Home catchall
-$Routes->add('home', Route::create('/index.html', 'App\Controller\Index::doDefault'));
-$Routes->add('home-base', Route::create('/', 'App\Controller\Index::doDefault'));
+$routes->add('home', Route::create('/index.html', 'App\Controller\Index::doDefault'));
+$routes->add('home-base', Route::create('/', 'App\Controller\Index::doDefault'));
 
-$Routes->add('login', Route::create('/login.html', 'App\Controller\Login::doInsLogin'));
-$Routes->add('institution-login', Route::create('/inst/{instHash}/login.html', 'App\Controller\Login::doInsLogin'));
-$Routes->add('admin-login', Route::create('/xlogin.html', 'App\Controller\Login::doDefault'));
+$routes->add('login', Route::create('/login.html', 'App\Controller\Login::doInsLogin'));
+$routes->add('institution-login', Route::create('/inst/{instHash}/login.html', 'App\Controller\Login::doInsLogin'));
+$routes->add('admin-login', Route::create('/xlogin.html', 'App\Controller\Login::doDefault'));
 
-$Routes->add('recover', Route::create('/recover.html', 'App\Controller\Recover::doDefault'));
-$Routes->add('install', Route::create('/install.html', 'App\Controller\Install::doDefault'));
+$routes->add('recover', Route::create('/recover.html', 'App\Controller\Recover::doDefault'));
+$routes->add('install', Route::create('/install.html', 'App\Controller\Install::doDefault'));
 
 // Admin Pages
-$Routes->add('admin-dashboard', Route::create('/admin/index.html', 'App\Controller\User\AdminDashboard::doDefault'));
-$Routes->add('admin-dashboard-base', Route::create('/admin/', 'App\Controller\User\AdminDashboard::doDefault'));
-$Routes->add('admin-settings', Route::create('/admin/settings.html', 'App\Controller\User\AdminSettings::doDefault'));
-$Routes->add('admin-institution-edit', Route::create('/admin/institutionEdit.html', 'App\Controller\Institution\Edit::doDefault'));
+$routes->add('admin-dashboard', Route::create('/admin/index.html', 'App\Controller\User\AdminDashboard::doDefault'));
+$routes->add('admin-dashboard-base', Route::create('/admin/', 'App\Controller\User\AdminDashboard::doDefault'));
+$routes->add('admin-settings', Route::create('/admin/settings.html', 'App\Controller\User\AdminSettings::doDefault'));
+$routes->add('admin-institution-edit', Route::create('/admin/institutionEdit.html', 'App\Controller\Institution\Edit::doDefault'));
 
 // Uni user tpoe Client Pages (not pathology clients)
 //$Routes->add('client-dashboard', Route::create('/client/index.html', 'App\Controller\Client\Dashboard::doDefault'));
 //$Routes->add('client-dashboard-base', Route::create('/client/', 'App\Controller\Client\Dashboard::doDefault'));
-$Routes->add('client-dashboard', Route::create('/client/index.html', 'App\Controller\User\ClientDashboard::doDefault'));
-$Routes->add('client-dashboard-base', Route::create('/client/', 'App\Controller\User\ClientDashboard::doDefault'));
-$Routes->add('client-settings', Route::create('/client/settings.html', 'App\Controller\Institution\Edit::doDefault'));
+$routes->add('client-dashboard', Route::create('/client/index.html', 'App\Controller\User\ClientDashboard::doDefault'));
+$routes->add('client-dashboard-base', Route::create('/client/', 'App\Controller\User\ClientDashboard::doDefault'));
+$routes->add('client-settings', Route::create('/client/settings.html', 'App\Controller\Institution\Edit::doDefault'));
 
 
 
 // Staff Pages
-$Routes->add('staff-dashboard', Route::create('/staff/index.html', 'App\Controller\Staff\Dashboard::doDefault'));
-$Routes->add('staff-dashboard-base', Route::create('/staff/', 'App\Controller\Staff\Dashboard::doDefault'));
-$Routes->add('staff-subject-dashboard', Route::create('/staff/{subjectCode}/index.html', 'App\Controller\Staff\SubjectDashboard::doDefault'));
-$Routes->add('staff-institution-edit', Route::create('/staff/settings.html', 'App\Controller\Institution\Edit::doDefault'));
+$routes->add('staff-dashboard', Route::create('/staff/index.html', 'App\Controller\Staff\Dashboard::doDefault'));
+$routes->add('staff-dashboard-base', Route::create('/staff/', 'App\Controller\Staff\Dashboard::doDefault'));
+$routes->add('staff-subject-dashboard', Route::create('/staff/{subjectCode}/index.html', 'App\Controller\Staff\SubjectDashboard::doDefault'));
+$routes->add('staff-institution-edit', Route::create('/staff/settings.html', 'App\Controller\Institution\Edit::doDefault'));
 
 
 
-$Routes->add('cassette-manager', Route::create('/staff/cassetteManager.html', 'App\Controller\Cassette\Manager::doDefault'));
-$Routes->add('cassette-edit', Route::create('/staff/cassetteEdit.html', 'App\Controller\Cassette\Edit::doDefault'));
+$routes->add('cassette-manager', Route::create('/staff/cassetteManager.html', 'App\Controller\Cassette\Manager::doDefault'));
+$routes->add('cassette-edit', Route::create('/staff/cassetteEdit.html', 'App\Controller\Cassette\Edit::doDefault'));
 
-$Routes->add('contact-manager', Route::create('/staff/contactManager.html', 'App\Controller\Contact\Manager::doDefault'));
-$Routes->add('contact-edit', Route::create('/staff/contactEdit.html', 'App\Controller\Contact\Edit::doDefault'));
+$routes->add('contact-manager', Route::create('/staff/contactManager.html', 'App\Controller\Contact\Manager::doDefault'));
+$routes->add('contact-edit', Route::create('/staff/contactEdit.html', 'App\Controller\Contact\Edit::doDefault'));
 
-$Routes->add('storage-manager', Route::create('/staff/storageManager.html', 'App\Controller\Storage\Manager::doDefault'));
-$Routes->add('storage-edit', Route::create('/staff/storageEdit.html', 'App\Controller\Storage\Edit::doDefault'));
+$routes->add('storage-manager', Route::create('/staff/storageManager.html', 'App\Controller\Storage\Manager::doDefault'));
+$routes->add('storage-edit', Route::create('/staff/storageEdit.html', 'App\Controller\Storage\Edit::doDefault'));
 
-$Routes->add('service-manager', Route::create('/staff/serviceManager.html', 'App\Controller\Service\Manager::doDefault'));
-$Routes->add('service-edit', Route::create('/staff/serviceEdit.html', 'App\Controller\Service\Edit::doDefault'));
+$routes->add('service-manager', Route::create('/staff/serviceManager.html', 'App\Controller\Service\Manager::doDefault'));
+$routes->add('service-edit', Route::create('/staff/serviceEdit.html', 'App\Controller\Service\Edit::doDefault'));
 
-$Routes->add('path-case-manager', Route::create('/staff/pathCaseManager.html', 'App\Controller\PathCase\Manager::doDefault'));
-$Routes->add('path-case-edit', Route::create('/staff/pathCaseEdit.html', 'App\Controller\PathCase\Edit::doDefault'));
+$routes->add('path-case-manager', Route::create('/staff/pathCaseManager.html', 'App\Controller\PathCase\Manager::doDefault'));
+$routes->add('path-case-edit', Route::create('/staff/pathCaseEdit.html', 'App\Controller\PathCase\Edit::doDefault'));
 
-$Routes->add('mail-template-manager', Route::create('/staff/mailTemplateManager.html', 'App\Controller\MailTemplate\Manager::doDefault'));
-$Routes->add('mail-template-edit', Route::create('/staff/mailTemplateEdit.html', 'App\Controller\MailTemplate\Edit::doDefault'));
+$routes->add('mail-template-manager', Route::create('/staff/mailTemplateManager.html', 'App\Controller\MailTemplate\Manager::doDefault'));
+$routes->add('mail-template-edit', Route::create('/staff/mailTemplateEdit.html', 'App\Controller\MailTemplate\Edit::doDefault'));
 
-$Routes->add('request-manager', Route::create('/staff/requestManager.html', 'App\Controller\Request\Manager::doDefault'));
-$Routes->add('request-edit', Route::create('/staff/requestEdit.html', 'App\Controller\Request\Edit::doDefault'));
+$routes->add('request-manager', Route::create('/staff/requestManager.html', 'App\Controller\Request\Manager::doDefault'));
+$routes->add('request-edit', Route::create('/staff/requestEdit.html', 'App\Controller\Request\Edit::doDefault'));
 
 
 
