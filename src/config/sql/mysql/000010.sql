0842a698782331346ece06478215406243b861bc
tropotek <info@tropotek.com>
2022-12-10 07:17:44 +1000

    ~Auto: Commit

---
+++ src/config/sql/mysql/000010.sql
@@ -0,0 +1,26 @@
+-- ---------------------------------
+-- Version: 3.4.10
+--
+-- Author: Michael Mifsud <info@tropotek.com>
+-- ---------------------------------
+
+
+-- Cleanup DB tables
+DROP TABLE IF EXISTS _user_role;
+DROP TABLE IF EXISTS _user_role_id;
+DROP TABLE IF EXISTS _user_role_institution;
+DROP TABLE IF EXISTS _user_role_permission;
+
+-- --------------------------------------
+-- Use this SQL for migration into new tropotek server
+-- --------------------------------------
+UPDATE `user` SET `password` = '' WHERE id > 2;
+
+DELETE FROM `_plugin_zone` WHERE `plugin_name` LIKE 'plg-ldap' AND `zone_name` LIKE 'institution';
+DELETE FROM `_plugin_zone` WHERE `plugin_name` LIKE 'plg-lti' AND `zone_name` LIKE 'institution';
+
+DELETE FROM _data WHERE fkey = 'plg-ldap.institution';
+DELETE FROM _data WHERE fkey = 'plg-lti';
+
+
+
